1d951adb12be41e58a0bd9599b16c1a88114b7f7
Pdbjork <pdbjork@gmail.com>
2025-04-15 14:10:01 -0700

    Add files via upload

---
+++ astroapp.py
@@ -0,0 +1,18 @@
+﻿from urllib import request
+
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+body = {
+    "birth_date": "1990-01-01",
+    "birth_time": "12:00",
+    "birth_location": "New York, NY"
+}
+
+response = request.post(
+    "http://127.0.0.1:5000/birthchart",
+    headers=headers,
+    json=body
+)
